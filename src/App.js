1c360c739424fa8dc470860ba1ad36d183915af4
gabrillukita1 <gabrillukita@gmail.com>
2022-03-23 05:13:28 +0700

    Add short card album

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import './App.css';
+import singleData from './assets/data/single'
 
 function App() {
-  return (
-    <div className="App">
-      <body>
-        <button class="button-Login">Log In Spotify</button>
-      </body>
-    </div>
-  );
+    const { album, artists } = singleData;
+    
+    return ( 
+        <main>
+            <div className='shortCard'>
+                <picture className='shortCard-image'>
+                    <source 
+                        media={`(min-width: ${album.images[0].width})`}
+                        srcSet={ album.images[0].url }
+                    />
+                    <source 
+                        media={`(min-width: ${album.images[1].width})`}
+                        srcSet={ album.images[1].url }
+                    />
+                    <source 
+                        media={`(min-width: ${album.images[2].width})`}
+                        srcSet={ album.images[2].url }
+                    />
+                    <img src={ album.images[1].url } alt='' />
+                </picture>
+                <div className='shortCard-desc'>
+                    <p>Album</p>
+                    <h2>{ album.name }</h2>
+                    <h4>{ artists[0].name }</h4>
+                    {/* <p>{`Release on ${album.release_date}`}</p> */}
+                </div>
+                <button className='btn btn-select'>Select</button>
+            </div>
+        </main>
+    );
 }
 
 export default App;
