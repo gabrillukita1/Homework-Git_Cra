8c5fae9ab7425a59beeff9f434663231a4832fb3
gabrillukita1 <gabrillukita@gmail.com>
2022-04-01 16:13:03 +0700

    State Lifecycle and Add Select Action -revision

--- src/App.js
+++ src/App.js
@@ -1,46 +1,46 @@
-import { Component } from 'react';
+import { Component, useState } from 'react';
 import './App.css';
 import ShortcardAlbum from './components/album';
 import SearchBar from './components/navbar/searchBar/searchBar';
 import Login from './components/navbar/login/login';
 
-class App extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            musics: [],
-        }
-    }
+// class App extends Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             musics: [],
+//         }
+//     }
 
-    render() {
-        const params = new URLSearchParams(window.location.hash.replace("#", "?"))
-        const accessToken = params.get("access_token")
+//     render() {
+//         const params = new URLSearchParams(window.location.hash.replace("#", "?"))
+//         const accessToken = params.get("access_token")
 
-        return (
-            <div>
-                <Login />
-                <SearchBar token={accessToken} onChange={(value)=> this.setState({
-                    musics: value
-                })} />
-                <ShortcardAlbum musics={this.state.musics} />
-            </div>
-        )
-    }
-}
-
-// const App = () => {
-//     const [ musics, setMusics ] = useState([]);
-
-//     const params = new URLSearchParams(window.location.hash.replace("#", "?"));
-//     const accessToken = params.get("access_token");
-   
-//     return (
-//         <div>
-//             <Login />
-//             <SearchBar token={ accessToken } onChange={(value) => setMusics({value})} />
-//             <ShortcardAlbum musics={ musics } />
-//         </div>
-//     ) 
+//         return (
+//             <div>
+//                 <Login />
+//                 <SearchBar token={accessToken} onChange={(value)=> this.setState({
+//                     musics: value
+//                 })} />
+//                 <ShortcardAlbum musics={this.state.musics} />
+//             </div>
+//         )
+//     }
 // }
+
+const App = () => {
+    const [ musics, setMusics ] = useState([]);
+
+    const params = new URLSearchParams(window.location.hash.replace("#", "?"));
+    const accessToken = params.get("access_token");
+   
+    return (
+        <div>
+            <Login />
+            <SearchBar token={ accessToken } onChange={(value) => setMusics(value)} />
+            <ShortcardAlbum musics={ musics } />
+        </div>
+    ) 
+}
 
 export default App;
