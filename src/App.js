7eefe0e70220c63daf856c9546692035844a30c6
gabrillukita1 <gabrillukita@gmail.com>
2022-04-02 18:49:02 +0700

    Add form playlist

--- src/App.js
+++ src/App.js
@@ -1,32 +1,9 @@
-import { Component, useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import ShortcardAlbum from './components/album';
 import SearchBar from './components/navbar/searchBar/searchBar';
 import Login from './components/navbar/login/login';
-
-// class App extends Component {
-//     constructor(props) {
-//         super(props)
-//         this.state = {
-//             musics: [],
-//         }
-//     }
-
-//     render() {
-//         const params = new URLSearchParams(window.location.hash.replace("#", "?"))
-//         const accessToken = params.get("access_token")
-
-//         return (
-//             <div>
-//                 <Login />
-//                 <SearchBar token={accessToken} onChange={(value)=> this.setState({
-//                     musics: value
-//                 })} />
-//                 <ShortcardAlbum musics={this.state.musics} />
-//             </div>
-//         )
-//     }
-// }
+import FormPlaylist from './components/playlist/formPlaylist';
 
 const App = () => {
     const [ musics, setMusics ] = useState([]);
@@ -37,6 +14,7 @@ const App = () => {
     return (
         <div>
             <Login />
+            <FormPlaylist />
             <SearchBar token={ accessToken } onChange={(value) => setMusics(value)} />
             <ShortcardAlbum musics={ musics } />
         </div>
---
+++ src/components/playlist/formPlaylist.js
@@ -0,0 +1,40 @@
+import React, { useState } from "react";
+import "./formPlaylist.css";
+
+const FormPlaylist = ({
+    defaultValue = {
+        name: "",
+        description: "",
+    },
+    onSubmit,
+}) => {
+    const [form, setForm] = useState(defaultValue);
+
+    // Remove event default and callback to onSubmit
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        onSubmit(form);
+    };
+
+    return (
+        <form className="form-playlist" onSubmit={handleSubmit}>
+            <div class="form">
+                <div class="title">Playlist</div>
+                <div class="subtitle">Let's create your playlist!</div>
+                <div class="input-container ic1">
+                    <input id="playlist-name" class="input" type="text" minLength={10} placeholder=" " />
+                    <div class="cut"></div>
+                    <label for="playlist-name" class="placeholder">Your playlist name..</label>
+                </div>
+                <div class="input-container ic2">
+                    <input id="description" class="input" type="text" placeholder=" " />
+                    <div class="cut"></div>
+                    <label for="description" class="placeholder">Description..</label>
+                </div>
+                <button type="text" class="submit">submit</button>
+            </div>
+        </form>
+    );
+};
+
+export default FormPlaylist;
