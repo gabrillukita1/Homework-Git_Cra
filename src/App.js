025ea8ae75039e5480a08177a52281f2b3e42509
gabrillukita1 <gabrillukita@gmail.com>
2022-04-13 05:50:15 +0700

    Change shortCard display layout

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import { useEffect, useState } from 'react';
 import './App.css';
+import React, { useEffect, useState } from 'react';
 import ShortcardAlbum from './components/album';
 import SearchBar from './components/navbar/searchBar/searchBar';
 import Login from './components/navbar/login/login';
 import FormPlaylist from './components/playlist/formPlaylist';
 import { useDispatch, useSelector } from 'react-redux';
 import { setToken } from './redux/globalSlice';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 const App = () => {
-    const [ musics, setMusics ] = useState([]);
-    const [ selected, setSelected ] = useState([]);
-    const [ profile, setProfile ] = useState({});
+    const [musics, setMusics] = useState([]);
+    const [selected, setSelected] = useState([]);
+    const [profile, setProfile] = useState({});
     const accessToken = useSelector((state) => state.global.accessToken)
     console.log(accessToken)
 
@@ -19,10 +21,11 @@ const App = () => {
     const dispatch = useDispatch();
 
     useEffect(() => {
-        dispatch(setToken(tokenParams))}, 
+        dispatch(setToken(tokenParams))
+    },
         [tokenParams]
     );
-   
+
     const getUserProfile = async () => {
         const response = await fetch(`https://api.spotify.com/v1/me`,
             {
@@ -35,32 +38,32 @@ const App = () => {
         console.log(data);
         setProfile(data);
     }
-    useEffect(() => {getUserProfile()}, [accessToken]);
+    useEffect(() => { getUserProfile() }, [accessToken]);
 
     const createPlaylist = async (value) => {
         const response = await fetch(`https://api.spotify.com/v1/users/${profile.id}/playlists`,
-        {
-            method: "POST",
-            headers: {
-                Authorization: `Bearer ${accessToken}`
-            },
-            body: JSON.stringify( { 
-                name: value.name, 
-                description: value.description,
-                public: false,
-                collaborative: false,
+            {
+                method: "POST",
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                },
+                body: JSON.stringify({
+                    name: value.name,
+                    description: value.description,
+                    public: false,
+                    collaborative: false,
+                })
             })
-        })
         const playlist = await response.json();
-        
+
         const playlistResponse = await fetch(`https://api.spotify.com/v1/playlists/${playlist.id}/tracks`,
-        {
-            method: "POST",
-            headers: {
-                Authorization: `Bearer ${accessToken}`
-            },
-            body: JSON.stringify({ uris: selected })
-        })
+            {
+                method: "POST",
+                headers: {
+                    Authorization: `Bearer ${accessToken}`
+                },
+                body: JSON.stringify({ uris: selected })
+            })
         const tracks = await playlistResponse.json()
         console.log(tracks)
     }
@@ -71,9 +74,15 @@ const App = () => {
             <Login />
             <FormPlaylist onSubmit={createPlaylist} />
             <SearchBar onChange={(value) => setMusics(value)} />
-            <ShortcardAlbum musics={ musics } selected={ selected } setSelected={ setSelected } />
+            <ShortcardAlbum musics={musics} selected={selected} setSelected={setSelected} />
+
+            {/* <Router>
+                <Route path="/" exact component={Login} />
+                <Route path="/" exact component={<SearchBar onChange={(value) => setMusics(value)} />} />
+                <Route path="/" exact component={<ShortcardAlbum musics={musics} selected={selected} setSelected={setSelected} />} />
+            </Router> */}
         </div>
-    ) 
+    )
 }
 
 export default App;
--- src/components/album/index.js
+++ src/components/album/index.js
@@ -1,13 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import "./album.css";
 import Image from "./image";
 import Desc from "./desc";
 
-
-
 // Album Wrapper
 const Album = ({ musics, selected, setSelected }) => {
-    
+
     const HandleSelected = (uri) => {
         const alreadySelected = selected.find((selectedUri) => selectedUri === uri);
         if (alreadySelected) {
@@ -20,18 +18,18 @@ const Album = ({ musics, selected, setSelected }) => {
         }
     }
 
-    
     return (
         <div className="album-wrapper">
             {musics.map(({ id, album, artists, name, uri }) => {
                 const isSelected = selected.find((selectedUri) => selectedUri === uri);
-                return(
-                <div className='shortCard'>
-                <Image album={album} />
-                <Desc album={album} title={name} artists={artists} />
-                <button className='btn btn-select' onClick={() => HandleSelected(uri)} >{ isSelected? "Diselect" : "Select" }</button>
-            </div>
-            )})}
+                return (
+                    <div className='shortCard'>
+                        <Image album={album} />
+                        <Desc album={album} title={name} artists={artists} />
+                        <button className='btn btn-select' onClick={() => HandleSelected(uri)} >{isSelected ? "Diselect" : "Select"}</button>
+                    </div>
+                )
+            })}
         </div>
     );
 };
